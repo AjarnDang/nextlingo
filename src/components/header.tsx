181fae1520954a6a02cf66120846662169ede915
wiwsoos <torntan.j@gmail.com>
2024-11-11 22:52:18 +0700

    [UPDATE] Project config with language change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,40 @@
-import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import LocalSwitcher from './local-switcher';
+import { useTranslations } from "next-intl";
+import LocalSwitcher from "./local-switcher";
+import Image from "next/image";
+import {
+  Button,
+  NavbarLink,
+  Navbar,
+  NavbarCollapse,
+  NavbarBrand,
+  NavbarToggle,
+} from "flowbite-react";
+import Logo from "@/app/assets/logo.png";
 
 export default function Header() {
-  const t = useTranslations('Navigation');
+  const t = useTranslations("Navigation");
 
   return (
-    <header className='p-4'>
-      <nav className='flex items-center justify-between'>
-        <Link href='/'>{t('home')}</Link>
+    <Navbar fluid rounded>
+      <NavbarBrand href="/">
+        <Image src={Logo} width={50} height={50} alt="DABUZZZZ Logo" />
+      </NavbarBrand>
+      
+      <NavbarCollapse>
+        <NavbarLink href="/" active>
+          {t("home")}
+        </NavbarLink>
+        <NavbarLink href="#">{t("service")}</NavbarLink>
+        <NavbarLink href="#">{t("gallery")}</NavbarLink>
+        <NavbarLink href="#">{t("about")}</NavbarLink>
+        <NavbarLink href="#">{t("contact")}</NavbarLink>
+      </NavbarCollapse>
+
+      <div className="flex md:order-2 gap-4">
         <LocalSwitcher />
-      </nav>
-    </header>
+        <Button className="rounded-full">{t("book")}</Button>
+        <NavbarToggle />
+      </div>
+    </Navbar>
   );
 }
--- src/components/local-switcher.tsx
+++ src/components/local-switcher.tsx
@@ -1,32 +1,53 @@
-'use client';
+"use client";
 
-import { useLocale } from 'next-intl';
-import { useRouter } from 'next/navigation';
-import { ChangeEvent, useTransition } from 'react';
+import { useLocale } from "next-intl";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useTransition, useState } from "react";
+import { GlobalOutlined } from "@ant-design/icons";
+import { Dropdown } from "flowbite-react";
 
 export default function LocalSwitcher() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const localActive = useLocale();
+  const [selectedLanguage, setSelectedLanguage] = useState(localActive);
 
-  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextLocale = e.target.value;
+  const changeLanguage = (newLocale) => {
+    setSelectedLanguage(newLocale);
     startTransition(() => {
-      router.replace(`/${nextLocale}`);
+      router.replace(`/${newLocale}`);
     });
   };
+
+  const languageLabel = selectedLanguage === "en" ? "EN" : "TH";
+
   return (
-    <label className='border-2 rounded'>
-      <p className='sr-only'>change language</p>
-      <select
+    <Dropdown
+      inline
+      arrowIcon={false}
+      label={
+        <div className="flex items-center">
+          <GlobalOutlined />
+          <span className="ml-2">{languageLabel}</span>
+        </div>
+      }
+    >
+      {/* <p className='sr-only'>change language</p> 
+       <select
         defaultValue={localActive}
-        className='bg-transparent py-2'
+        className="bg-transparent py-2"
         onChange={onSelectChange}
         disabled={isPending}
       >
-        <option value='en'>English</option>
-        <option value='id'>Indonesian</option>
-      </select>
-    </label>
+        <option value="en">English</option>
+        <option value="id">Indonesian</option>
+      </select> */}
+      <Dropdown.Item onClick={() => changeLanguage("en")} disabled={isPending}>
+        English
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => changeLanguage("th")} disabled={isPending}>
+        Thai
+      </Dropdown.Item>
+    </Dropdown>
   );
 }
